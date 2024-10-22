5717ac0e69c63dc75fe36fd0ea623f9944436f08
천세경 <jay45652@gmail.com>
2024-10-22 15:25:05 +0900

    [BE]settings : init

---
+++ ssaffice/src/main/java/com/jetty/ssaffice/SsafficeApplication.java
@@ -0,0 +1,13 @@
+package com.jetty.ssaffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsafficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsafficeApplication.class, args);
+	}
+
+}
