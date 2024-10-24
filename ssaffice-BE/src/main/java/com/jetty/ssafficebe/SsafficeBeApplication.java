0f3fd43781fc4d157fca5791687b52693f894185
천세경 <jay45652@gmail.com>
2024-10-24 17:28:23 +0900

    [BE]settings : Backend init

---
+++ ssaffice-BE/src/main/java/com/jetty/ssafficebe/SsafficeBeApplication.java
@@ -0,0 +1,13 @@
+package com.jetty.ssafficebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsafficeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsafficeBeApplication.class, args);
+	}
+
+}
