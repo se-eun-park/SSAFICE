f3b0874002644da7e9402c1ffd7be359b72de86a
천세경 <jay45652@gmail.com>
2024-10-25 10:12:14 +0900

    [BE]feat : add UserRole Entity

---
+++ ssaffice-BE/src/main/java/com/jetty/ssafficebe/role/entity/UserRole.java
@@ -0,0 +1,34 @@
+package com.jetty.ssafficebe.role.entity;
+
+import com.jetty.ssafficebe.user.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roleId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "roleId", insertable = false, updatable = false)
+    private Role role;
+
+    private Long userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", insertable = false, updatable = false)
+    private User user;
+}
