8ecb772787540c96811cef33c7c8e880ad22567d
김해엽 <rjssod2@naver.com>
2024-10-28 11:32:50 +0900

    [BE]feat : schedule controller 추가

---
+++ ssaffice-BE/src/main/java/com/jetty/ssafficebe/schedule/controller/ScheduleController.java
@@ -0,0 +1,38 @@
+package com.jetty.ssafficebe.schedule.controller;
+
+import com.jetty.ssafficebe.schedule.service.ScheduleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/schedule")
+@RequiredArgsConstructor
+public class ScheduleController {
+
+    private final ScheduleService scheduleService;
+
+    /**
+     * 일정 등록
+     */
+
+    /**
+     * 일정 수정
+     */
+
+    /**
+     * 일정 리스트 조회
+     */
+
+    /**
+     * 일정 조회
+     */
+
+    /**
+     * 일정 삭제
+     */
+
+    /**
+     * 일정 검색
+     */
+}
