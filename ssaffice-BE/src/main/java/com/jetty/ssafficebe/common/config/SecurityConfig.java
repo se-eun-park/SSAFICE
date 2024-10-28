a066843b5c7003ef3de7c8ad2ee8d4d5f6da4040
천세경 <jay45652@gmail.com>
2024-10-28 13:16:18 +0900

    [BE]feat : 테스트를 위한 SecurityConfig 추가

---
+++ ssaffice-BE/src/main/java/com/jetty/ssafficebe/common/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.jetty.ssafficebe.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers(
+                                "/",
+                                "/home",
+                                "/error",
+                                "/api/**"
+                        ).permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
