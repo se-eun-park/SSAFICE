3b4eb58be26fd0948814e0dde80330b0f4e431d4
박세은 <seeunbag30@gmail.com>
2024-10-29 16:52:30 +0900

    [FE]chore: svgr 플러그인 설치 및 적용

--- ssaffice-FE/vite.config.ts
+++ ssaffice-FE/vite.config.ts
@@ -1,10 +1,18 @@
 import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
   server: {
     port: 8080,
     open: true,
