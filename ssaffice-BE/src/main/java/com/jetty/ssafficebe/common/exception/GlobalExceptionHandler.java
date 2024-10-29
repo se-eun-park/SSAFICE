f01af6a6605548bcaf8265e7538b207d7ebb6d5b
천세경 <jay45652@gmail.com>
2024-10-29 16:41:26 +0900

    [BE]fix : DuplicatedValueException 발생 시 ok로 나오는 문제 해결

--- ssaffice-BE/src/main/java/com/jetty/ssafficebe/common/exception/GlobalExceptionHandler.java
+++ ssaffice-BE/src/main/java/com/jetty/ssafficebe/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.jetty.ssafficebe.common.exception;
 import com.jetty.ssafficebe.common.exception.exceptiontype.DuplicateValueException;
 import com.jetty.ssafficebe.common.payload.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,9 +14,10 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(DuplicateValueException.class)
     protected ResponseEntity<ApiResponse> handleDuplicateValueException(DuplicateValueException e) {
+        HttpStatus status = e.getErrorCode().getStatus();
 
-        return ResponseEntity.status(e.getErrorCode().getStatus())
-                             .body(new ApiResponse(false, e.getMessage(), e.getData()));
+        return ResponseEntity.status(status)
+                             .body(new ApiResponse(false, status, e.getMessage(), e.getData()));
     }
 
 }
