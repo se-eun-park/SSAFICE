25dbb7dd50a8d75d6aff275054d0b18ef5c47338
천세경 <jay45652@gmail.com>
2024-10-30 14:00:56 +0900

    [BE]test : updateUser http 테스트 코드 추가

--- ssaffice-BE/src/main/java/com/jetty/ssafficebe/user/payload/UpdateUserRequest.java
+++ ssaffice-BE/src/main/java/com/jetty/ssafficebe/user/payload/UpdateUserRequest.java
@@ -1,12 +1,12 @@
 package com.jetty.ssafficebe.user.payload;
 
-import jakarta.persistence.criteria.CriteriaBuilder.In;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UpdateUserRequest {
+
     private String name;
 
     private Integer cohortNum;
