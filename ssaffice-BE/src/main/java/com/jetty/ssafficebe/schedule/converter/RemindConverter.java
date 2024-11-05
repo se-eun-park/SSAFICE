b682f1c4a07aa9848d60e6768259d0c18f4c4b5a
김해엽 <rjssod2@naver.com>
2024-11-05 19:23:57 +0900

    [BE]fix : converter 수정

--- ssaffice-BE/src/main/java/com/jetty/ssafficebe/schedule/converter/RemindConverter.java
+++ ssaffice-BE/src/main/java/com/jetty/ssafficebe/schedule/converter/RemindConverter.java
@@ -2,19 +2,11 @@ package com.jetty.ssafficebe.schedule.converter;
 
 import com.jetty.ssafficebe.schedule.entity.Remind;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE, unmappedTargetPolicy = ReportingPolicy.WARN)
 public interface RemindConverter {
 
-    Remind toRemind(LocalDateTime remindDateTimes);
-
-    default List<Remind> toRemindList(List<LocalDateTime> remindDateTimes) {
-        return remindDateTimes.stream()
-                              .map(this::toRemind)
-                              .collect(Collectors.toList());
-    }
+    Remind toRemind(LocalDateTime remindDateTime);
 }
