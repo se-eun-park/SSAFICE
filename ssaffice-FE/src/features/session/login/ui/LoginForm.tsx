62993fc2815d45668fd8826155e3330d20bb4ff6
이주영 <jylee9931@gmail.com>
2024-11-05 14:29:26 +0900

    [FE] feat: 변경된 기획 반영된 login 화면 구현

--- ssaffice-FE/src/features/session/login/ui/LoginForm.tsx
+++ ssaffice-FE/src/features/session/login/ui/LoginForm.tsx
@@ -6,22 +6,44 @@ export const LoginForm = () => {
       <div className='flex flex-col gap-spacing-64'>
         <div className='flex flex-col gap-spacing-24'>
           <div className='flex flex-col gap-spacing-12'>
-            <div className='flex justify-start text-color-text-tertiary body-md-semibold'>
+            <div
+              className='
+              flex justify-start 
+              text-color-text-tertiary body-md-semibold
+              '
+            >
               이메일
             </div>
             <input
               type='email'
-              className='flex w-full border border-color-border-secondary rounded-radius-8 placeholder:body-md-medium placeholder:color-text-disabled py-spacing-12 px-spacing-16'
+              className='
+              flex 
+              w-full px-spacing-16 py-spacing-12
+              border border-color-border-secondary 
+              rounded-radius-8 
+              placeholder:color-text-disabled placeholder:body-md-medium 
+              '
               placeholder='EMAIL'
+              required
             />
           </div>
           <div className='flex flex-col gap-spacing-12'>
-            <div className='flex justify-start text-color-text-tertiary body-md-semibold'>
+            <div
+              className='
+            flex justify-start 
+            text-color-text-tertiary body-md-semibold
+            '
+            >
               비밀번호
             </div>
             <input
               type='password'
-              className='flex w-full border border-color-border-secondary rounded-radius-8 placeholder:body-md-medium placeholder:color-text-disabled py-spacing-12 px-spacing-16'
+              className='
+              flex 
+              w-full px-spacing-16 py-spacing-12 
+              border border-color-border-secondary 
+              rounded-radius-8 
+              placeholder:color-text-disabled placeholder:body-md-medium'
               placeholder='PASSWORD'
             />
           </div>
@@ -29,20 +51,32 @@ export const LoginForm = () => {
         <LoginButton label='로그인' />
       </div>
 
-      {/* 선 위의 글자 작업 중 */}
-      <div className='relative flex flex-col items-center w-full h-spacing-32'>
-        <div className='flex justify-center items-center w-full border-t border-color-text-disabled'>
-          {/* 선을 CSS로 구현 */}
-        </div>
-        <span className='absolute bg-white body-sm-medium text-color-text-disabled z-10'>
+      {/* 
+          SSAFY 로그인을 진행하지 않게 되어 임시 주석 처리합니다.
+      <div
+        className='
+      flex flex-col justify-center items-center 
+      w-full h-spacing-32
+      '
+      >
+        <span
+          className='
+        flex 
+        text-color-text-disabled body-sm-medium
+        bg-white 
+        '
+        >
           또는 다음으로 계속
         </span>
       </div>
 
       <LoginButton
         label='SSAFY 계정으로 로그인'
-        classname='border bg-color-bg-primary border-color-border-primary text-color-text-interactive-secondary'
-      />
+        classname='
+        text-color-text-interactive-secondary
+        bg-color-bg-primary border border-color-border-primary 
+        '
+      /> */}
     </div>
   )
 }
