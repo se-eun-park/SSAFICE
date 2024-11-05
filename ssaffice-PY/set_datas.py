0cfa2f6824443583cf5b14d66a1255a07793dc0f
김재현 <wogus565@gmail.com>
2024-11-05 18:01:36 +0900

    [PY]feat : db접근 메서드 작성

--- ssaffice-PY/set_datas.py
+++ ssaffice-PY/set_datas.py
@@ -1,6 +1,24 @@
 from setup import get_db
+from models.models import Notice, Notice_Channel
 
-def insert_notice(notice):  
+def insert_notice(notice):    
   with get_db() as db: 
     db.add(notice)
+    db.commit()
+    return notice.notice_id
+
+def insert_notice_channel(notice_channel):
+  with get_db() as db:
+    db.add(notice_channel)
+    db.commit()
+    return notice_channel.notice_channel_id
+    
+def insert_notice_channel_from_notice_id(notice_id, channel_id, mm_team_id):
+  with get_db() as db:
+    notice_channel = Notice_Channel(
+      notice_id = notice_id,
+      channel_id = channel_id,
+      mm_team_id = mm_team_id
+    )
+    db.add(notice_channel)
     db.commit()
