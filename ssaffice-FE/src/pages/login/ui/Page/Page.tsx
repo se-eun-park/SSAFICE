efc0e5669756e56a5c41a0806205b1707005c9d9
이주영 <jylee9931@gmail.com>
2024-11-06 10:32:03 +0900

    [FE] fix: 로그인 폼 너비 값 제대로 반영, 단위 rem으로 변환

--- ssaffice-FE/src/pages/login/ui/Page/Page.tsx
+++ ssaffice-FE/src/pages/login/ui/Page/Page.tsx
@@ -3,7 +3,7 @@ import { LoginForm } from '@/features/session/login'
 export const LoginPage = () => {
   return (
     <div className='flex flex-col items-center w-full'>
-      <div className='flex flex-col items-center w-[500px]'>
+      <div className='flex flex-col items-center w-[28.375rem]'>
         <div className='flex flex-col w-full mt-spacing-128'>
           <h1 className='self-center text-color-text-primary heading-desktop-3xl w-fit px-spacing-64 mb-spacing-64'>
             SSAFICE 시작하기
