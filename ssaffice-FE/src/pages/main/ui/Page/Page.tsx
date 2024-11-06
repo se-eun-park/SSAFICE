17a1ea96bc01e23852b105e1c0fff7c27f48caf1
박세은 <seeunbag30@gmail.com>
2024-11-06 17:07:56 +0900

    [FE]feat: 구현한 위젯들 Main페이지에 연결, DropDown 컴포넌트 hover 색상 변경

--- ssaffice-FE/src/pages/main/ui/Page/Page.tsx
+++ ssaffice-FE/src/pages/main/ui/Page/Page.tsx
@@ -1,9 +1,20 @@
 import { SummaryTab } from '@/widgets/summaryTab'
+import { AnnouncementTab } from '@/widgets/announcementTab'
+import { TodoTab } from '@/widgets/todoTab'
+import { useIsTabOpenStore, useIsAnimationStore } from '@/shared/model'
 
 export const MainPage = () => {
+  const isTabOpen = useIsTabOpenStore()
+  const isAnimation = useIsAnimationStore()
+
   return (
-    <main className='w-full h-full px-spacing-32 py-spacing-40'>
+    <main className='flex flex-col w-full h-full gap-y-spacing-32 px-spacing-32 py-spacing-40'>
       <SummaryTab />
+
+      <div className={`flex gap-x-spacing-40 ${isAnimation ? ' animate-gapShrink' : ''}`}>
+        {isTabOpen && <AnnouncementTab />}
+        <TodoTab />
+      </div>
     </main>
   )
 }
