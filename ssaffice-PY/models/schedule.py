19cd0c73a6fed44b7bb855ace003afc952fa8391
김재현 <wogus565@gmail.com>
2024-11-07 18:44:56 +0900

    [PY] feat: schedule 모델 작성

---
+++ ssaffice-PY/models/schedule.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, String, Integer, ForeignKey, Boolean, Enum, DateTime
+from sqlalchemy.orm import relationship, backref
+from .type import *
+from .base import Base
+
+class Schedule(Base):
+    __tablename__ = "schedule"
+
+    schedule_id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(255))
+    memo = Column(String(500))
+    start_date_time = Column(DateTime(timezone=True))
+    end_date_time = Column(DateTime(timezone=True))
+    task_type = Column(Enum(TaskType), nullable=True)
+    schedule_source_type = Column(Enum(ScheduleSourceType), nullable=True)
+    schedule_status_type = Column(
+        Enum(ScheduleStatusType), default=ScheduleStatusType.TODO, nullable=True
+    )
+    is_essential = Column(BooleanToYN, default="N")
+    is_enroll = Column(BooleanToYN, default="N")
+    user_id = Column(Integer, ForeignKey("user.user_id"), nullable=True)   
+    notice_id = Column(Integer, ForeignKey("notice.notice_id"), nullable=True)
+
+class Remind(Base):
+    __tablename__ = "remind"
+
+    remind_id = Column(Integer, primary_key=True, autoincrement=True)
+    is_essential = Column(BooleanToYN, default="N")
+    time = Column(DateTime(timezone=True))
+    schedule_id = Column(Integer, ForeignKey("schedule.schedule_id"), nullable=False)
