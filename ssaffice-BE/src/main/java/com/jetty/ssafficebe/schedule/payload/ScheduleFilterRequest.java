d1833ddcadc3faa4a05fdf84d896d4902af2914e
김해엽 <rjssod2@naver.com>
2024-11-08 16:22:50 +0900

    [BE]feat : 일정 리스트 필터 dto 및 queryDSL 추가

---
+++ ssaffice-BE/src/main/java/com/jetty/ssafficebe/schedule/payload/ScheduleFilterRequest.java
@@ -0,0 +1,16 @@
+package com.jetty.ssafficebe.schedule.payload;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ScheduleFilterRequest {
+
+    private String isEnrollYn;
+    private String scheduleSourceTypeCd;
+    private String scheduleStatusTypeCd;
+    private LocalDateTime filterStartDateTime;
+    private LocalDateTime filterEndDateTime;
+}
---
+++ ssaffice-BE/src/main/java/com/jetty/ssafficebe/schedule/repository/ScheduleRepositoryCustom.java
@@ -0,0 +1,11 @@
+package com.jetty.ssafficebe.schedule.repository;
+
+import com.jetty.ssafficebe.schedule.entity.Schedule;
+import com.jetty.ssafficebe.schedule.payload.ScheduleFilterRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ScheduleRepositoryCustom {
+
+    Page<Schedule> getSchedulesByFilter(ScheduleFilterRequest scheduleFilterRequest, Pageable pageable);
+}
