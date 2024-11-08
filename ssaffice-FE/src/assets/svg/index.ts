687088e0df69a2579d86baf678fe146e26f751c6
이주영 <jylee9931@gmail.com>
2024-11-08 10:25:43 +0900

    [FE] fix: merge conflict fixed

--- ssaffice-FE/src/assets/svg/index.ts
+++ ssaffice-FE/src/assets/svg/index.ts
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 export { default as AlertLogo } from './AlertLogo'
 export { default as ExitButton } from './ExitButton'
-=======
 export { default as FastLeftArrowIcon } from './FastLeftArrowIcon'
 export { default as HamburgerMenuIcon } from './HamburgerMenuIcon'
->>>>>>> 5d35d7da6b26fcb6ea09ea0612b265fc5dd4d710
 export { default as Logo } from './Logo'
 export { default as LogoutIcon } from './LogoutIcon'
 export { default as NotificationInactiveIcon } from './NotificationInactiveIcon'
