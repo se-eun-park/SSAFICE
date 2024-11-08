d4780df09653dddbd0169a69a53b8fc759dd6956
김해엽 <rjssod2@naver.com>
2024-11-08 16:19:26 +0900

    [BE]feat : 일정 리스트 조회 응답 dto 생성

---
+++ ssaffice-BE/src/main/java/com/jetty/ssafficebe/schedule/payload/ScheduleSummaryForList.java
@@ -0,0 +1,19 @@
+package com.jetty.ssafficebe.schedule.payload;
+
+import com.jetty.ssafficebe.user.payload.CreatedBySummary;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ScheduleSummaryForList {
+
+    private String scheduleId;
+    private String title;
+    private LocalDateTime startDateTime;
+    private LocalDateTime endDateTime;
+    private String scheduleSourceTypeCd;
+    private String scheduleStatusTypeCd;
+    private CreatedBySummary createUser;
+}
