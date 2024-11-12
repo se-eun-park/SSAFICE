0769c72bbd7e7cfd465cf7506f77ccc7e776c715
이주영 <jylee9931@gmail.com>
2024-11-12 10:13:40 +0900

    [FE] fix: 메인 레이아웃 깨진 부분 수정 - height 속성 변경, 추후 제대로 수정 필요 (max-h-?? 속성으로)

--- ssaffice-FE/src/widgets/announcementTab/ui/AnnouncementTab.tsx
+++ ssaffice-FE/src/widgets/announcementTab/ui/AnnouncementTab.tsx
@@ -77,7 +77,7 @@ export const AnnouncementTab = () => {
         <div
           className={`
           flex
-          ${isAllNoticeView ? 'max-h-[734px]' : 'max-h-[806px] mt-spacing-24'} pb-spacing-24 px-spacing-16
+          ${isAllNoticeView ? 'mb-[132px]' : 'mt-spacing-24 mb-spacing-32'} px-spacing-16
           bg-color-bg-tertiary
           rounded-radius-8
           overflow-y-scroll
