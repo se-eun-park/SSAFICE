5aa163bdb0dc3059cef3bcfbec23ca3a2038a579
박세은 <seeunbag30@gmail.com>
2024-11-12 21:33:38 +0900

    [FE]docs: 더미데이터 수정

--- ssaffice-FE/src/features/todoTab/model/CardSourceTypeElements.ts
+++ ssaffice-FE/src/features/todoTab/model/CardSourceTypeElements.ts
@@ -1,29 +1,29 @@
 import { useMemo } from 'react'
 
 type CardSourceTypeElementProps = {
-  scheduleSourceType: string
+  scheduleSourceTypeCd: string
 }
 
-export const CardSourceTypeElements = ({ scheduleSourceType }: CardSourceTypeElementProps) =>
+export const CardSourceTypeElements = ({ scheduleSourceTypeCd }: CardSourceTypeElementProps) =>
   useMemo(() => {
-    switch (scheduleSourceType) {
-      case 'all':
+    switch (scheduleSourceTypeCd) {
+      case 'GLOBAL':
         return {
           description: '전체 공지에서 등록',
           classname:
             'bg-color-bg-info w-fit h-fit px-spacing-4 py-spacing-2 rounded-radius-4 text-color-text-interactive-inverse body-xs-semibold',
         }
-      case 'team':
+      case 'TEAM':
         return {
           description: '팀 공지에서 등록',
           classname:
             'bg-color-bg-warning w-fit h-fit px-spacing-4 py-spacing-2 rounded-radius-4 text-color-text-interactive-inverse body-xs-semibold',
         }
-      case 'my':
+      case 'PERSONAL':
         return {
           description: '내가 등록',
           classname:
             'bg-color-bg-success w-fit h-fit px-spacing-4 py-spacing-2 rounded-radius-4 text-color-text-interactive-inverse body-xs-semibold',
         }
     }
-  }, [scheduleSourceType])
+  }, [scheduleSourceTypeCd])
