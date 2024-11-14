525cc7cb662aac3d0bd6e6b4840beb8a7105e08b
김재현 <wogus565@gmail.com>
2024-11-14 11:44:28 +0900

    [BE] feat: matterMostToken이 없을 때 사용할 ErrorCode와 핸들러 추가

--- ssaffice-BE/src/main/java/com/jetty/ssafficebe/common/exception/ErrorCode.java
+++ ssaffice-BE/src/main/java/com/jetty/ssafficebe/common/exception/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
     NOTICE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 공지사항을 찾을 수 없습니다."),
     SCHEDULE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 일정을 찾을 수 없습니다."),
     REMIND_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 알림을 찾을 수 없습니다."),
+    TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 토큰을 찾을 수 없습니다."),
 
     EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 사용중인 이메일입니다."),
     ROLE_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 존재하는 역할입니다."),
