74925a6a18d842f1f371ec22b6b37026600986d3
천세경 <jay45652@gmail.com>
2024-11-17 01:03:27 +0900

    [BE]refactor : toChannelSummary Mapping 설정 변경

--- ssaffice-BE/src/main/java/com/jetty/ssafficebe/channel/converter/ChannelConverter.java
+++ ssaffice-BE/src/main/java/com/jetty/ssafficebe/channel/converter/ChannelConverter.java
@@ -4,11 +4,13 @@ import com.jetty.ssafficebe.channel.entity.Channel;
 import com.jetty.ssafficebe.channel.payload.ChannelSummary;
 import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE, unmappedTargetPolicy = ReportingPolicy.WARN)
 public interface ChannelConverter {
 
+    @Mapping(source = "mmTeam.mmTeamName", target = "mmTeamName")
     ChannelSummary toChannelSummary(Channel channel);
 
     List<ChannelSummary> toChannelSummaryList(List<Channel> channelList);
