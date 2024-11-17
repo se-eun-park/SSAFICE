3c6ac49b6ed77ddee7f572e41c5af92a1ba5cd25
천세경 <jay45652@gmail.com>
2024-11-17 17:00:54 +0900

    [BE]chore : 엔드포인트 수정

--- ssaffice-BE/src/main/java/com/jetty/ssafficebe/search/controller/ESNoticeController.java
+++ ssaffice-BE/src/main/java/com/jetty/ssafficebe/search/controller/ESNoticeController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/search/notice")
+@RequestMapping("/api/es/notice")
 @RequiredArgsConstructor
 public class ESNoticeController {
 
