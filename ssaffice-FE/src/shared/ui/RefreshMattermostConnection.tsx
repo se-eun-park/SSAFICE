4626f57d214da3f9b4fc92013cceaee1983eeaba
이주영 <jylee9931@gmail.com>
2024-11-19 00:17:38 +0900

    [FE] fix: MM 새로고침 버튼 영역 넓힘

--- ssaffice-FE/src/shared/ui/RefreshMattermostConnection.tsx
+++ ssaffice-FE/src/shared/ui/RefreshMattermostConnection.tsx
@@ -9,25 +9,25 @@ export const RefreshMattermostConnection = () => {
         flex gap-spacing-8 items-center
     '
     >
-      {/* refresh SVG(RefreshIcon)로 바꿔주세요. 데탑에서 svgr 작동 안 돼서 public/svg에 파일만 넣어둠 ㅜㅜ */}
-      <button
-        className='
+      <button className='flex gap-spacing-8 items-center' onClick={refreshMattermostConnect}>
+        <div
+          className='
         w-spacing-12 h-spacing-12
       '
-        onClick={refreshMattermostConnect}
-      >
-        <RefreshIcon />
-      </button>
+        >
+          <RefreshIcon />
+        </div>
 
-      <div
-        className='
+        <div
+          className='
         flex justify-start
         w-[160px]
         body-md-medium text-color-text-primary
       '
-      >
-        MM 팀/채널 새로고침
-      </div>
+        >
+          MM 팀/채널 새로고침
+        </div>
+      </button>
       <div
         className='
         flex justify-end
