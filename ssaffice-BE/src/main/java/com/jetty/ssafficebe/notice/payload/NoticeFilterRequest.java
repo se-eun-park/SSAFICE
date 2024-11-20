a4a67df630d3bcc9009be6d2a0ecf403009eee95
천세경 <jay45652@gmail.com>
2024-11-20 13:21:44 +0900

    [BE] refactor : getMyNoticeList POST -> GET으로 수정

--- ssaffice-BE/src/main/java/com/jetty/ssafficebe/notice/payload/NoticeFilterRequest.java
+++ ssaffice-BE/src/main/java/com/jetty/ssafficebe/notice/payload/NoticeFilterRequest.java
@@ -1,11 +1,13 @@
 package com.jetty.ssafficebe.notice.payload;
 
 import java.time.LocalDateTime;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class NoticeFilterRequest {
 
     private LocalDateTime filterStartDateTime;
