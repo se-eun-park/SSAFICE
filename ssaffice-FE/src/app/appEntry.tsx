a1d154bbe71f17bbc433895bdbdfc39268dd2eb3
kwakseongjae <gkffhdnls13@gmail.com>
2024-11-20 15:01:20 +0900

    [FE] feat: queryClient default 옵션 추가

--- ssaffice-FE/src/app/appEntry.tsx
+++ ssaffice-FE/src/app/appEntry.tsx
@@ -6,7 +6,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import './index.css'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
