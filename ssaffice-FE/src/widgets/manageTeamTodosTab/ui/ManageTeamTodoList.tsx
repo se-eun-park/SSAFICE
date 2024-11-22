425d3704483ebbcab7f60377f0b68106f0061278
이주영 <jylee9931@gmail.com>
2024-11-22 14:03:00 +0900

    [FE] fix: queryKey = teamTodos 파트 api 요청 post/body -> get/requestParam 방식으로 변경(start, end 형태 맞는지 확인해야 함)

--- ssaffice-FE/src/widgets/manageTeamTodosTab/ui/ManageTeamTodoList.tsx
+++ ssaffice-FE/src/widgets/manageTeamTodosTab/ui/ManageTeamTodoList.tsx
@@ -9,11 +9,9 @@ export const ManageTeamTodoList = () => {
   const { data, isLoading, error } = useQuery({
     queryKey: ['teamTodos'],
     queryFn: async () => {
-      const { data } = await instance.post('/api/notice/admin/my', {
-        filterStartDateTime: new Date('2024-01-01'),
-        filterEndDateTime: new Date(),
-        filterType: 'createdAt',
-      })
+      const { data } = await instance.get(
+        `/api/notice/admin/my?start=${'2024-01-01'}&end=${'2024-12-31'}&type=createdAt&sort=endDateTime,asc`,
+      )
       return data
     },
   })
