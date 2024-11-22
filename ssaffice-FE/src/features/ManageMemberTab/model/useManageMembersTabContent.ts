6a3c330b89eaaf5d306b88512c3fb362674295ac
이주영 <jylee9931@gmail.com>
2024-11-22 16:10:19 +0900

    [FE] feat: ManagerMembers content 구성 관련(channelId로 유저 리스트 불러오기) API 적용

--- ssaffice-FE/src/features/ManageMemberTab/model/useManageMembersTabContent.ts
+++ ssaffice-FE/src/features/ManageMemberTab/model/useManageMembersTabContent.ts
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import { dummySsafyUsers, MattermostChannel, SsafyUser, SsafyUserApiResponse } from './types'
 import { Pagenation } from '@/shared/model'
+import { useQuery } from '@tanstack/react-query'
+import { instance } from '@/shared/api'
 
 export const useManageMembersTabContent = (channel: MattermostChannel) => {
   const [userInChannelList, setUserInChannelList] = useState<SsafyUser[]>([])
@@ -8,21 +10,24 @@ export const useManageMembersTabContent = (channel: MattermostChannel) => {
   const [selectedAll, setSelectedAll] = useState(false)
   const [pageInfo, setPageInfo] = useState<Pagenation | null>(null)
 
+  // ====================== 여기 API 다는 걸로 수정해야 함 =====================
   const fetchAPI = async (
     channelId: string,
     pageNumber?: number,
   ): Promise<SsafyUserApiResponse> => {
-    String(channelId) // API 연동 후 삭제(오류 방지용 로직)
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        if (pageNumber === undefined) pageNumber = 0
-        resolve(dummySsafyUsers[pageNumber])
-      }, 1000)
-    })
+    const response = await instance.get(
+      `api/users/admin/${channelId}?page=${pageNumber === undefined ? 0 : pageNumber}&size=10&sort=name,asc`,
+    )
+    return response.data
   }
 
   const fetchUserInChannelList = async (pageNumber?: number) => {
     await fetchAPI(channel.channelId, pageNumber).then((res) => {
+      // await instance.get(
+      //   `/admin/12345?page=${pageNumber}&size=10&sort=name,asc`
+      // ).then((res) => {
+      console.log(res)
+      console.log(res.content)
       setUserInChannelList(res.content)
       if (res.pageable && res.totalPages !== undefined && res.totalElements !== undefined)
         setPageInfo({
@@ -33,6 +38,7 @@ export const useManageMembersTabContent = (channel: MattermostChannel) => {
         })
     })
   }
+  // ===========================================
 
   useEffect(() => {
     fetchUserInChannelList()
