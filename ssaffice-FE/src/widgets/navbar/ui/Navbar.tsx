85d59b34b75f6bdcd8fa1acd89bb7ac35f9d9ea0
kwakseongjae <gkffhdnls13@gmail.com>
2024-12-03 17:23:51 +0900

    [FE] feat: navbar 로고 클릭 시 라우팅 기능 추가

--- ssaffice-FE/src/widgets/navbar/ui/Navbar.tsx
+++ ssaffice-FE/src/widgets/navbar/ui/Navbar.tsx
@@ -5,13 +5,20 @@ import {
 } from '@/features/navbar'
 import { Logo } from '@/assets/svg'
 import { useLoginStateStore } from '@/entities/session/index.ts'
+import { useNavigate } from 'react-router-dom'
 
 export const Navbar = () => {
   const isAuthenticated = useLoginStateStore()
+  const navigate = useNavigate()
 
   return (
-    <header className='sticky top-0 z-30 flex items-center justify-between w-full border-b px-spacing-80 bg-color-bg-primary h-[76px] py-spacing-16 border-color-border-tertiary'>
-      <Logo className='h-9' />
+    <header className='sticky top-0 z-30 flex items-center justify-between w-full border-b px-spacing-40 md:px-spacing-80 bg-color-bg-primary h-[76px] py-spacing-16 border-color-border-tertiary'>
+      <Logo
+        className='cursor-pointer h-9'
+        onClick={() => {
+          !isAuthenticated && navigate('/')
+        }}
+      />
       {isAuthenticated ? (
         <div className='flex items-center gap-x-spacing-40'>
           {/* <ClickNotificationButton /> */}
