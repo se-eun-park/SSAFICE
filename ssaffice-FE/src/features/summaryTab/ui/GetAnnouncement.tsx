c98d1f56dfce501bdba0ca821f12b4543b517d0f
이주영 <jylee9931@gmail.com>
2024-12-15 18:39:27 +0900

    [FE] fix: import문 수정 / summary 공지 영역 기본값으로 0 세팅

--- ssaffice-FE/src/features/summaryTab/ui/GetAnnouncement.tsx
+++ ssaffice-FE/src/features/summaryTab/ui/GetAnnouncement.tsx
@@ -9,21 +9,23 @@ type GetAnnouncementProps = {
 }
 
 export const GetAnnouncement = ({ noticeCounts }: GetAnnouncementProps) => {
+  const totalCounts = noticeCounts?.total ?? 0
+  const essentialCounts = noticeCounts?.essential ?? 0
+  const enrolledCounts = noticeCounts?.enrolled ?? 0
+
   return (
     <div>
       <h2 className='heading-desktop-lg mb-spacing-4 text-color-text-primary min-w-max'>공지</h2>
 
       <ul className='flex flex-col border-t px-spacing-4 py-spacing-10 border-color-border-info gap-y-spacing-10 text-color-text-primary'>
         <li className='flex items-center body-md-medium min-w-max'>
-          전체 <span className='ml-spacing-8'>{noticeCounts?.total}</span>
+          전체 <span className='ml-spacing-8'>{totalCounts}</span>
         </li>
         <li className='flex items-center body-md-medium min-w-max'>
-          중요{' '}
-          <span className='ml-spacing-8 text-color-text-danger'>{noticeCounts?.essential}</span>
+          중요 <span className='ml-spacing-8 text-color-text-danger'>{essentialCounts}</span>
         </li>
         <li className='flex items-center body-md-medium min-w-max'>
-          등록{' '}
-          <span className='ml-spacing-8 text-color-text-info-bold'>{noticeCounts?.enrolled}</span>
+          등록 <span className='ml-spacing-8 text-color-text-info-bold'>{enrolledCounts}</span>
         </li>
       </ul>
     </div>
